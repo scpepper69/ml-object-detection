10b08eb73d06f8caddb310a474b1526192584cf6
scpepper69 <secret.crunch.pepper@gmail.com>
2019-04-17 23:06:21 +0900

    samples

---
+++ camera/camera_test.py
@@ -0,0 +1,12 @@
+import cv2
+cam = cv2.VideoCapture(0)
+while True:
+    # 画像を取得 --- (*2)
+    _, img = cam.read()
+    # ウィンドウに画像を表示 --- (*3)
+    cv2.imshow('PUSH ENTER KEY', img)
+    # Enterキーが押されたら終了する
+    if cv2.waitKey(1) == 13: break
+# 後始末
+cam.release()
+cv2.destroyAllWindows()
